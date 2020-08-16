ac5550ac73bb26ad4d84d916388110af216dd3dc
Karsten Taylor <karstentaylor0@gmail.com>
2020-08-16 15:39:20 -0700

    commit

--- src/FolderViewMain.js
+++ src/FolderViewMain.js
@@ -2,30 +2,26 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import propTypes from 'prop-types';
 import NotefulContext from './NotefulContext';
-import config from './config';
 
 function FolderViewMain(props) {
-    const deleteNoteRequest = (id, deleteNoteFromUI) => {
-        fetch(`${config.API_ENDPOINT}/api/notes/${id}`, {
-            'method': 'DELETE',
-            'headers': {
-                'Authorization': `Bearer ${config.API_KEY}`,
-            }
-        })
+    const deleteNoteRequest = (noteId, deleteNoteFromUI) => {
+        fetch(`http://localhost:9090/notes/${noteId}`, { 'method': 'DELETE' })
             .then(response => response.json())
-            .then(response => deleteNoteFromUI(id))
+            .then(response => deleteNoteFromUI(noteId))
             .then(props.history.push('/'))
     };
 
     return (
         <NotefulContext.Consumer>
             {value => {
-                const notes = value.notes.filter(note => note.folder_name === Number(props.match.params.folder_id)).map(note => {
+                const notes = value.STORE.notes.filter(note => note.folderId === props.match.params.folderId).map(note => {
+                    let dateNoteModifiedObj = new Date(note.modified);
+                    let dateNoteModified = dateNoteModifiedObj.toDateString();
                     return (
                         <section className="border group-column note-margin note-padding width" key={note.id}>
-                            <h2><Link to={`/note/${note.id}`}>Note Name: {note.note_name}</Link></h2>
+                            <h2><Link to={`/note/${note.id}`}>Name: {note.name}</Link></h2>
                             <div className="group-row note-group-row">
-                                <p>Date modified on: {new Date(note.modified).toLocaleString()}</p>
+                                <p>Date modified on: {dateNoteModified}</p>
                                 <button onClick={() => deleteNoteRequest(note.id, value.deleteNoteFromUi)}>Delete Note</button>
                             </div>
                         </section>
